0aa8c2c3ebf878afcd48862aa72a3fd355241424
Gabriel <129018468+GabrielxBarreto@users.noreply.github.com>
2026-06-01 20:14:05 -0300

    atualizando a base

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -5,11 +5,17 @@ from django.contrib import admin
 from dashboard import models
 
 @admin.register(models.Streaming)
-
 class StreamingAdmin(admin.ModelAdmin):
     list_display = 'id','name','url','categoria'
 
 @admin.register(models.Plano)
-
 class PlanoAdmin(admin.ModelAdmin):
     list_display = 'id','name','preco_mensal','qualidade_video','ativo','quantidade_telas','anuncio'
+
+@admin.register(models.Participante)
+class ParticipanteAdmin(admin.ModelAdmin):
+    list_display = 'id','name','email','data_cadastro','status'
+
+@admin.register(models.Grupo)
+class GrupoAdmin(admin.ModelAdmin):
+    list_display = 'id','name','descricao','plano','streaming'
--- dashboard/models.py
+++ dashboard/models.py
@@ -9,6 +9,7 @@ class Streaming(models.Model):
 
     def __str__(self):
         return self.name
+    
 class Plano(models.Model):
     name = models.CharField(max_length=45)
     preco_mensal = models.FloatField()
@@ -16,3 +17,18 @@ class Plano(models.Model):
     ativo = models.BooleanField()
     quantidade_telas = models.IntegerField()
     anuncio = models.BooleanField()
+
+class Participante(models.Model):
+    name = models.CharField(max_length=45)
+    email=  models.EmailField()
+    data_cadastro= models.DateField()
+    status = models.BooleanField() 
+
+class Grupo(models.Model):
+    name = models.CharField(max_length=45)
+    descricao = models.CharField(max_length=255)
+    plano = models.ForeignKey(Plano, on_delete=models.CASCADE)
+    streaming = models.ForeignKey(Streaming, on_delete=models.CASCADE)
+
+
+    
