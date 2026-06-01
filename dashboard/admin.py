c5cefb410ef18af9ebbb5d9bb5e0428cfe12d267
Gabriel <129018468+GabrielxBarreto@users.noreply.github.com>
2026-06-01 17:01:00 -0300

    adicionando a entidade plano

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -5,5 +5,11 @@ from django.contrib import admin
 from dashboard import models
 
 @admin.register(models.Streaming)
+
 class StreamingAdmin(admin.ModelAdmin):
-    list_display = 'id','name'
+    list_display = 'id','name','url','categoria'
+
+@admin.register(models.Plano)
+
+class PlanoAdmin(admin.ModelAdmin):
+    list_display = 'id','name','preco_mensal','qualidade_video','ativo','quantidade_telas','anuncio'
--- dashboard/models.py
+++ dashboard/models.py
@@ -3,7 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Streaming(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=45)
+    url = models.CharField(max_length=255,default="https://teste.com")
+    categoria = models.CharField(max_length=128,default="Video")
 
     def __str__(self):
         return self.name
+class Plano(models.Model):
+    name = models.CharField(max_length=45)
+    preco_mensal = models.FloatField()
+    qualidade_video = models.CharField(max_length=30)
+    ativo = models.BooleanField()
+    quantidade_telas = models.IntegerField()
+    anuncio = models.BooleanField()
