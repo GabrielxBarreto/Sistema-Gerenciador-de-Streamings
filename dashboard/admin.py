8dd5480d0f0b6adc190fcc49bc13a63f6f97aa7e
Gabriel <129018468+GabrielxBarreto@users.noreply.github.com>
2026-06-16 12:32:28 -0300

    Suporte completo HBO max

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -15,7 +15,7 @@ class PlanoAdmin(admin.ModelAdmin):
 
 @admin.register(models.Participante)
 class ParticipanteAdmin(admin.ModelAdmin):
-    list_display = 'id','username','email','data_cadastro','status'
+    list_display = 'id','username','password','email','data_cadastro','status'
 
 @admin.register(models.Grupo)
 class GrupoAdmin(admin.ModelAdmin):
